21cbe7ef7eca039c107295f110a4cb986b287d41
lgklsv <kolosov20011@gmail.com>
2022-09-11 21:43:52 +0300

    feat: add theme switcher prototype

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import './styles/main.scss';
 
 console.log('hello wrld');
+
+const toggleSwitch = document.querySelector('#themeSwither');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    } 
+};
+
+toggleSwitch.addEventListener('change', switchTheme, false);
